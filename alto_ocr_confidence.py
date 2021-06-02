673dc0ffdf8744acd44c0e8216179ae218eff6a2
Karl <kraegelin@sub.uni-goettingen.de>
2021-06-02 12:48:42 +0200

    error handling and confiddance calculation for whole folder

--- alto_ocr_confidence.py
+++ alto_ocr_confidence.py
@@ -9,35 +9,42 @@ namespace = {'alto-1': 'http://schema.ccs-gmbh.com/ALTO',
              'alto-2': 'http://www.loc.gov/standards/alto/ns-v2#',
              'alto-3': 'http://www.loc.gov/standards/alto/ns-v3#'}
 
-def parse_alto(fh):
-    tree = ET.parse(fh)
-    score = 0
-    count = 0
-    xmlns = tree.getroot().tag.split('}')[0].strip('{')  # extract namespace from root
-    if xmlns in namespace.values():
-        for elem in tree.iterfind('.//{%s}String' % xmlns):
-                wc = elem.attrib.get('WC')
-                score += float(wc)
-                count += 1
-        if count > 0:
-            confidence = score / count
-            result = round(100 * confidence, 2)
-            sys.stdout.write('\nFile: %s, Confidence: %s' % (fh.name, result))
-        else:
-            sys.stdout.write('\nFile: %s, Confidence: 00.00' % (fh.name))
+def parse_alto(fpath, f):
+    try:
+        tree = ET.parse(fpath)
+    except ET.ParseError as e:
+        print(f"Parser Error in file '{fpath}': {e}")
     else:
-        sys.stdout.write('\nERROR: File "%s" does not appear to be a valid ALTO file (namespace declaration missing)' % fh.name)
+        score = 0
+        count = 0
+        xmlns = tree.getroot().tag.split('}')[0].strip('{')  # extract namespace from root
+        if xmlns in namespace.values():
+            for elem in tree.iterfind('.//{%s}String' % xmlns):
+                    wc = elem.attrib.get('WC')
+                    if wc is not None:   
+                        score += float(wc)
+                        count += 1
+            if count > 0:
+                confidence = score / count
+                result = round(100 * confidence, 2)
+                sys.stdout.write(f'\nFile: {f}, Confidence: {result}')
+                return result
+            else:
+                sys.stdout.write(f'\nFile: {f}, Confidence: 00.00')
+                return 0
+        else:
+            sys.stdout.write(f'\nERROR: File "{f}" does not appear to be a valid ALTO file (namespace declaration missing)')
+            return None
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python %s <inputdir>" % os.path.basename(__file__))
         sys.exit(-1)
-
-    for root, dirs, files in os.walk(sys.argv[1]):
-        for filename in files:
-            if filename.endswith('.xml') or filename.endswith('.alto'):
-                fh = open(os.path.join(root, filename), 'r', encoding='UTF8')
-                for f in fh:
-                    parse_alto(fh)
-                fh.close()
+    files = [f for f in os.listdir(sys.argv[1]) if f.endswith('xml') or f.endswith('.alto')]
+    conf = []
+    for f in files:
+        fpath = os.path.join(sys.argv[1], f)
+        conf.append(parse_alto(fpath, f))
+    
+    print(f"\n\nConfidence of folder: {round(sum([e for e in conf if e is not None])/len(files), 2)}")
